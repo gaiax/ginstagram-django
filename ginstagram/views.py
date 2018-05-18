fa844f8ec89c47c2202c3b57cbc383ecb76464c4
godan <shogo.matsushita@gaiax.com>
2018-05-18 11:39:45 +0900

    [add] ユーザー情報を取得する処理を追加

--- ginstagram/views.py
+++ ginstagram/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .forms import ContactForm
 from django.http.response import HttpResponse
+from .models import Users
 
 def main(request):
     return HttpResponse("Hello!")
@@ -15,10 +16,11 @@ def login(request):
     return render(request, template, {
         'form': form,
     })
- 
+
 def profile(request, user_name):
+    userInfo = get_object_or_404(Users, username=user_name)
     return render(
-               request, 
-               'ginstagram/profile.html', 
-               {'userInfo': user_name} 
+               request,
+               'ginstagram/profile.html',
+               {'userInfo': userInfo}
             )
