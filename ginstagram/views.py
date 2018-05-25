416b6f4853e15969bbb2baffdb148107c8ad48c3
hiroaki.iwabuchi <hiroaki.iwabuchi@adish.co.jp>
2018-05-25 18:04:02 +0900

    [delete] forms.pyを削除

--- ginstagram/views.py
+++ ginstagram/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
-from .forms import ContactForm
 from django.http.response import HttpResponse
 from .models import Users
 
