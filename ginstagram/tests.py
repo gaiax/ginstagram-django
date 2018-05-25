d906dd2cf10bf3395210f9c283048140b8014f1d
hiroaki.iwabuchi <hiroaki.iwabuchi@adish.co.jp>
2018-05-25 16:02:39 +0900

    [refactor] 不要なテストの整理

--- ginstagram/tests.py
+++ ginstagram/tests.py
@@ -1,23 +1,17 @@
-from django.http import HttpRequest
-from django.urls import resolve, reverse
+from django.urls import reverse
 from django.test import TestCase
-from ginstagram.views import main
 from .models import Users
 
-class UrlResolveTests(TestCase):
-    def test_url_resolves_to_myapp_view(self):
-        match = resolve('/')
-        self.assertEqual(match.func, main)
-    def test_main_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = main(request)
-        expected_html = 'Hello!'
-        self.assertEqual(response.content.decode(), expected_html)
 
-class 既存の動き(TestCase):
+class ユーザー詳細表示機能(TestCase):
 
     def test_ユーザー詳細ページのレスポンスにusernameが含まれている(self):
-        user = Users.objects.create(username='TEST_USER_NAME', icon='image/image.jpg')
-        response = self.client.get(reverse('ginstagram:profile', args=[user.username]))
+        user = Users.objects.create(
+            username='TEST_USER_NAME',
+            icon='image/image.jpg',
+        )
+        response = self.client.get(
+            reverse('ginstagram:profile', args=[user.username])
+        )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, user.username)
