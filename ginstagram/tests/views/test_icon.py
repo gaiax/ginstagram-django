7e32149d745b33dccdcbd456464de81ef541dcd5
hiroaki.iwabuchi <hiroaki.iwabuchi@adish.co.jp>
2018-05-31 14:36:27 +0900

    [recfactor] テストメソッドの位置を一部変更

--- ginstagram/tests/views/test_icon.py
+++ ginstagram/tests/views/test_icon.py
@@ -12,21 +12,6 @@ from ginstagram.forms import UserIconForm
 
 class ユーザーアイコン編集画面表示機能(TestCase):
 
-    def test_ユーザープロフィール画面にプロフィール編集画面へのリンクを表示すること(self):
-        user = User.objects.create(
-            username='TEST_USER_NAME',
-            icon='image/image.jpg',
-        )
-        response = self.client.get(
-            reverse('ginstagram:profile', args=[user.username])
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'プロフィール画像編集')
-        self.assertContains(
-            response,
-            reverse('ginstagram:icon', kwargs={'username': user.username})
-        )
-
     def test_画像アップロード用のフォームが表示されること(self):
         user = User.objects.create(
             username='TEST_USER_NAME',
